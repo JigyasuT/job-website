58553ba137c97e30df0fcf86f3b624238ce20272
JigyasuT <jigyasuti@gmail.com>
2025-09-19 09:57:10 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // ✅ For form data (URL encoded)
 // app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://job-website-1-c4h3.onrender.com/",
   credentials: true,
 };
 app.use(cors(corsOptions));
